7ca4d69864b8724f29ffd8f29a0366a01b1acad0
CNompiles <xristosnompiles16@gmail.com>
2025-09-07 09:45:49 +0300

    first unity commit

---
+++ Assets/Slug.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class Slug : MonoBehaviour
+{
+    public int damage = 1; // Πόση ζημιά κάνει το slug
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeHit(damage); // Στέλνουμε damage στον Enemy
+            }
+
+            Destroy(gameObject); // Το slug εξαφανίζεται μετά τη σύγκρουση
+        }
+    }
+}

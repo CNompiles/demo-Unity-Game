bff761a31b4c46de357534a9b9d16249395398f1
CNompiles <xristosnompiles16@gmail.com>
2025-09-13 02:40:23 +0300

    add playershooting

---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [Header("Slugs")]
+    public GameObject[] slugs;
+    public Transform firePoint; //το σημειο που θα εμφανιζει το slug
+    public float shootForce = 20f;
+
+    private int currentSlugIndex = 0; //Ποιο slug ειναι ενεργο 
+
+    void Update()
+    {
+        //Αλλαγη του slug 
+        if (Input.GetKeyDown(KeyCode.Alpha1)) currentSlugIndex = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha1)) currentSlugIndex = 2;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject slug = Instantiate(slugs[currentSlugIndex], firePoint.position, firePoint .rotation);
+        Rigidbody rb = slug.GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.forward * shootForce, ForceMode.Impulse);
+    }
+}                                                                  

dadd84223c7625e2562b0a1c6c2f021969779287
CNompiles <xristosnompiles16@gmail.com>
2025-09-27 11:41:29 +0300

    delete rigidbody and greate Charactercontroller and edit script playermovement with Cha/Contr.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;   // Ταχύτητα κίνησης
-    private Rigidbody rb;
+    public float moveSpeed = 5f;   // Ταχύτητα κίνησης
+    public float gravity = -9.81f; // βαρυτητα
+    public float jumpHeight = 1.5f; // Αλμα 
+    private CharacterController controller;
+    private Vector3 velocity; 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal"); // A/D ή ← →
-        float moveZ = Input.GetAxis("Vertical");   // W/S ή ↑ ↓
-        Vector3 movement = new Vector3(moveX, 0, moveZ);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        float x = Input.GetAxis("Horizontal"); // A/D ή ← →
+        float z = Input.GetAxis("Vertical");   // W/S ή ↑ ↓
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+        if (controller.isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
 
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }

7e5beaf11561ed82012c2cb4c21b216f66c36203
CNompiles <xristosnompiles16@gmail.com>
2025-09-29 11:12:02 +0300

    create script enemybullet

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float damage = 10f;
+    public float lifetime = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth ph = GetComponent<PlayerHealth>();
+            if (ph != null)
+            {
+                Destroy(gameObject);
+            }
+            else if (!other.isTrigger)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
